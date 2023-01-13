acdffa309b9a4d031aef40dcd92ef67fe82f975b
unknown <hasnainpriam925@gmail.com>
2023-01-13 11:10:43 +0600

    project setup

---
+++ src/firebase.init.js
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app";
+
+// Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAa5wRFy0VZVdcLvVoPVOhMOPW5Xw_8dvc",
+  authDomain: "doctors-portal-6cec1.firebaseapp.com",
+  projectId: "doctors-portal-6cec1",
+  storageBucket: "doctors-portal-6cec1.appspot.com",
+  messagingSenderId: "379284861204",
+  appId: "1:379284861204:web:b0bbaf9465fb5bf71b1201"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
